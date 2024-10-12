0c8ddc1600809fc4869274d689bff6b5438b72dd
Eric Johnson <ericdmjohnson@gmail.com>
2024-10-12 08:35:48 -0400

    updating js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+//initial popup window
+window.addEventListener("load", function () {
+  this.setTimeout(
+      function open(event) {
+          document.querySelector(".popup").style.display = "block";
+      },
+      0
+  )
+});
+
 //popup window interactions
 document.querySelector("#close").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "none";
